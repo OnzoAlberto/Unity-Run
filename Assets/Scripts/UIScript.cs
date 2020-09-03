7ad92cc7390314529b715eecd692c7a4ada46601
Alberto <onzoalberto@gmail.com>
2020-09-03 18:14:42 +0200

    Add asset folder

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public static bool pause = false;
+    float time;
+    public GameObject Score, EndGameCanvas,FinalScore;
+    private static GameObject finalScore;
+
+    void Start()
+    {
+        
+        Time.timeScale = 0;
+        finalScore = FinalScore;
+    }
+
+    void Update()
+    {
+        if(pause)
+        Score.GetComponent<Text>().text = "Score: " + ((int)(Time.timeSinceLevelLoad*100)).ToString();
+
+        if (Input.GetKey("p"))
+            StopGame();
+
+        if (Input.GetKey("r"))
+            ResumeGame();
+    }
+
+    public void startgame()
+    {
+        Time.timeScale = 1;
+        pause = true;
+    }
+
+    public void StopGame()
+    {
+        time = Time.timeScale;
+        Time.timeScale = 0;
+        pause = false;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale =1+ time ;
+        pause = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void EndGame()
+    {
+        Destroy(GameObject.Find("model"));
+        GameObject.Find("Canvas").transform.GetChild(2).gameObject.SetActive(true);
+       pause = false;
+       finalScore.GetComponent<Text>().text = "Your score is: \n " + ((int)(Time.timeSinceLevelLoad * 100)).ToString();
+
+    }
+}
---
+++ Assets/Scripts/manager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manager : MonoBehaviour
+{
+    Animator PlayerAnimator;
+    BoxCollider colliderForJump;
+    CapsuleCollider colliderForSlide;
+    public GameObject Placeholder1,Placeholder2, life;
+    public GameObject[] obstacles;
+    private int LifePoint = 3;
+   
+
+
+    private GameObject obj = null;
+
+    private void Start()
+    {
+        PlayerAnimator = this.GetComponent<Animator>();
+        colliderForJump = this.GetComponent<BoxCollider>();
+        colliderForSlide = this.GetComponent<CapsuleCollider>();
+    }
+
+
+    private void Update()
+    {
+        if(UIScript.pause)
+        Time.timeScale = Time.timeScale + 0.0001f;
+
+        if ((Input.GetKey("up") || Input.GetKey("w")) && this.transform.localPosition.y < 1)
+           {
+            PlayerAnimator.Play("up");
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y + 0.1f, 4);
+           }
+       
+        if ((Input.GetKey("down") || Input.GetKey("s")) && this.transform.localPosition.y > -1)
+            {
+            PlayerAnimator.Play("down");
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y - 0.1f, 4);
+            }
+   
+        if ((Input.GetKey("left") || Input.GetKey("a")) && this.transform.localPosition.x > -1.5)
+        {
+            PlayerAnimator.Play("left");
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x - 0.1f, this.transform.localPosition.y, 4);
+            }
+
+        if ((Input.GetKey("right") || Input.GetKey("d")) && this.transform.localPosition.x < 1.5)
+        {
+            PlayerAnimator.Play("right");
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x + 0.1f, this.transform.localPosition.y, 4);
+        }
+
+    }
+
+    private void spawnObstacle()
+    {
+        Destroy(obj,.5f);
+
+        var random = Random.Range(0, obstacles.Length);
+
+        obj = Instantiate(obstacles[random],Placeholder1.transform,false);
+    }
+
+    private void spawnObstacle2()
+    {
+        Destroy(obj,.5f);
+
+        var random = Random.Range(0, obstacles.Length);
+
+        obj= Instantiate(obstacles[random], Placeholder2.transform, false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag.Equals("Obstacle"))
+        {
+            PlayerAnimator.Play("collision");
+            if (LifePoint > 0)
+            {
+                LifePoint--;
+                life.GetComponent<Text>().text = "Life: " + LifePoint.ToString();
+                other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                other.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                Destroy(other.transform.parent.gameObject, 2f);
+            }
+
+            if (LifePoint== 0)
+
+            {
+                Destroy(other.transform.parent.gameObject);
+                UIScript.EndGame();
+            }
+        }
+
+        if (other.gameObject.tag.Equals("Bonus"))
+        {
+           // LifePoint++;
+            //life.GetComponent<Text>().text = "life: " + LifePoint.ToString();
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.name.Equals("Placeholder1") && (LifePoint > 0))
+            spawnObstacle2();
+        if (other.gameObject.name.Equals("Placeholder2") && (LifePoint > 0))
+            spawnObstacle();
+
+    }
+
+
+
+}
